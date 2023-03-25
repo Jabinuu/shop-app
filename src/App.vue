75780548a845a5622d2279b9115ba97ee820e4ed
jiabin <1123565780@qq.com>
2023-03-25 16:28:27 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// 引入组件
+import Header from "./components/Header/indexHeader";
+import Footer from "@/components/Footer/indexFooter";
 export default {
-  name: 'App',
+  name: "App",
+  // 注册组件
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Footer,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  // render、template、el这三个DOM选项要一起看
+  // render渲染函数里的形参h函数（即createElement()）负责创建一个VNode节点，
+  // 然后render渲染函数将该VNode渲染成为DOM节点
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app')   // $mount()作用是把这个Vue实例手动挂载到index.html（单页面程序的主页）的根节点下
